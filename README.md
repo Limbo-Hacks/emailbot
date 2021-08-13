9a3ecb7f75a354c86b664ccaeb6160fba6d55850
Zoheb Alli Khan <understandingzak@gmail.com>
2021-08-13 22:06:51 +0530

    Boilerplate

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mail bot
+
+A bot to send reminders and notifications to hackers about announcements.
+
+# Folder structure
+
+Source code are inside src directory.
+
+`announcements` -> `Sending mails for announcing or updates`.
+
+`workshops` -> `Sending mails for workshops`
+
+`reminders` -> `Reminding about how much time left for workshops and submitting time.`
+
+No external modules are required for now cuz we are using inbuilt mails.
---
+++ src/announcements/joinDiscord.py
@@ -0,0 +1,52 @@
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+port = 465
+sender_mail = input("Enter the mail address of limbohacks:")
+password = input("Input the password of limbohacks mail:")
+hacker_mails = input("Enter all the hacker mails:")
+
+message = MIMEMultipart("Limbo Hacks")
+message["Subject"] = "Join Discord"
+message["From"] = sender_mail
+message["To"] = hacker_mails
+
+head = """\
+    LimboHacks
+    """
+html = """\
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+<style>
+body {
+  background-color: black;
+  text-align: center;
+  color: white;
+}
+</style>
+</head>
+<body>
+
+<h1>Join our Discord</h1>
+<p>Join our <a href="https://discord.gg/8XJSzmtWPp">Discord</a> and meet amazing hackers and judge from globe</p>
+
+</body>
+</html>
+"""
+
+div1 = MIMEText(head, "plain")
+div2 = MIMEText(html, "html")
+
+message.attach(div1)
+message.attach(div2)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
+    server.login(sender_mail, password)
+    server.sendmail(sender_mail, hacker_mails, message.as_string())
+    print("Discord announcement sent")
